558616260f4c64debec7e9fe1c4ff47710c3eec4
charlesfeng207 <charlesfeng207@gmail.com>
2023-12-23 00:01:51 +0800

    123

--- test/strTest.cpp
+++ test/strTest.cpp
@@ -3,6 +3,7 @@
 #include <cstring>
 #include <iostream>
 #include <boost/format.hpp>
+#include <sstream>
 
 using namespace std;
 
@@ -29,11 +30,39 @@ int strcmptest(const char *s1, const char *s2)
     return *s1 == *s2 ? 1 : 0;
 }
 
+template<typename ... Args>
+static std::string str_format(const std::string &format, Args ... args)
+{
+	auto size_buf = std::snprintf(nullptr, 0, format.c_str(), args ...) + 1; 
+	std::unique_ptr<char[]> buf(new(std::nothrow) char[size_buf]);
+
+	if (!buf)
+		return std::string("");
+
+	std::snprintf(buf.get(), size_buf, format.c_str(), args ...);
+	return std::string(buf.get(), buf.get() + size_buf - 1); 
+}
+
+template<typename ... Args>
+static std::wstring wstr_format(const std::wstring &format, Args ... args)
+{
+	auto size_buf = std::snprintf(nullptr, 0, format.c_str(), args ...) + 1; 
+	std::unique_ptr<char[]> buf(new(std::nothrow) char[size_buf]);
+
+	if (!buf)
+		return std::wstring("");
+
+	std::snprintf(buf.get(), size_buf, format.c_str(), args ...);
+	return std::wstring(buf.get(), buf.get() + size_buf - 1); 
+}
+
 void formatString()
 {
     cout << "formatString..." << endl;
     std::string str = "1234567890"; 
     str += "x";
+    str += 2;
+    
     cout << str << endl;
     
     char* package_name = "com.tencent.tmgp.pubgmhd";
@@ -45,6 +74,20 @@ void formatString()
     fmt % package_name % 100;
     cout << fmt.str() << endl;
 
+    std::string strtest		= str_format("%d.%d.%d", 1, 0, 1);
+    std::string wstrtest	= str_format("%d.%d.%d", 2, 3, 2);
+
+    cout << "str = " << strtest << "\n";
+    cout << "wstr = " << wstrtest << "\n";
+
+    std::string name = "Bob";
+    int age = 30;
+
+    std::stringstream ss;
+    ss << "My name is " << name << " and I am " << age << " years old." << package_name;
+    std::string formattedString = ss.str();
+    std::cout << formattedString << std::endl;
+    
 }
 
 int main()
