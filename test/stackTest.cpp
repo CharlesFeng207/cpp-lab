bdefa736838990bb1e10384497658c2b61b09fa3
Charles <charlesfeng207@gmail.com>
2024-01-28 21:53:44 +0800

    123

--- test/stackTest.cpp
+++ test/stackTest.cpp
@@ -3,6 +3,7 @@
 class TestClass
 {
     public:
+        int num;
         TestClass()
         {
             std::cout << "Hello World!" << std::endl;
@@ -13,10 +14,24 @@ class TestClass
         };
 };
 
+TestClass TestFunction()
+{
+    TestClass test;
+    test.num = 5;
+    return test;
+}
+
 int main()
 {
     if(true)
-        TestClass test;
-    std::cout << "End of main" << std::endl;
+        TestClass test; // 离开作用域时，会调用析构函数
+    std::cout << "End of if" << std::endl;
+
+    // 返回值优化（Return Value Optimization，简称 RVO）是一种优化技术
+    // 用于避免不必要的对象拷贝操作。它的主要原理是将函数中创建的局部对象直接放置到函数调用者提供的内存空间中
+    // 从而避免了拷贝构造函数的调用。
+    auto t = TestFunction();
+    std::cout << "End of main " << t.num << std::endl;
+
     return 0;
 }
---
+++ test/vtbl_hack.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+
+class Component
+{
+public:
+    virtual void FunctionPad0()
+    {
+        std::cout << "FunctionPad"
+                  << "\n";
+    }
+    virtual void FunctionPad1()
+    {
+        std::cout << "FunctionPad"
+                  << "\n";
+    }
+    virtual void FunctionPad2()
+    {
+        std::cout << "FunctionPad"
+                  << "\n";
+    }
+    virtual void FunctionPad3()
+    {
+        std::cout << "FunctionPad"
+                  << "\n";
+    }
+    virtual void FunctionPad4()
+    {
+        std::cout << "FunctionPad"
+                  << "\n";
+    }
+    virtual void FunctionPad5()
+    {
+        std::cout << "FunctionPad"
+                  << "\n";
+    }
+    virtual void FunctionPad6()
+    {
+        std::cout << "FunctionPad"
+                  << "\n";
+    }
+    virtual void FunctionPad7()
+    {
+        std::cout << "FunctionPad"
+                  << "\n";
+    }
+    virtual void FunctionPad8()
+    {
+        std::cout << "FunctionPad"
+                  << "\n";
+    }
+    virtual void FunctionPad9()
+    {
+        std::cout << "FunctionPad"
+                  << "\n";
+    }
+    virtual const char *GetName()
+    {
+        return "Component";
+    }
+};
+
+class Animator : public Component
+{
+public:
+    const char* name = "Animator";
+    virtual const char *GetName()
+    {
+        return name;
+    }
+};
+
+class JobData
+{
+public:
+    char pad[24];
+    Animator *m_Animator = nullptr;
+    char pad2[110];
+};
+
+class HackData
+{
+public:
+    HackData()
+    {
+        std::cout << "Data Constructor won't call" << "\n";
+    }
+};
+
+int main()
+{
+    JobData *jobData = new JobData[2];
+    jobData[0].m_Animator = new Animator();
+    jobData[1].m_Animator = new Animator();
+
+    std::cout << sizeof(jobData[0]) << "\n";
+    // std::cout << jobData[0].m_Animator->GetName() << "\n";
+
+    int index = 1;
+    uintptr_t jobDataPtrAddr = (uintptr_t)jobData + 144 * index; // 计算第N个JobData的地址
+    uintptr_t animatorPtrAddr = *(uintptr_t*)(jobDataPtrAddr + 24); // 计算第N个JobData的Animator指针地址
+    uintptr_t vtablePtrAddr = *(uintptr_t *)animatorPtrAddr; // 对象首地址为虚表指针变量
+    uintptr_t *vtblPtr = (uintptr_t *)vtablePtrAddr; // vtable地址转换成指针
+    uintptr_t functionAddr = vtblPtr[10]; // 10 是 GetName 函数在 vtable 中的索引
+    
+    // 万能指针转换为对象指针，用于调用成员函数
+    HackData* animatorPtr = (HackData*)animatorPtrAddr;
+
+    // 定义一个函数指针类型，与被调用的成员函数签名匹配
+    typedef const char*(*fakefunc)(HackData &);
+
+    // 将万能指针转换为函数指针
+    fakefunc func = (fakefunc)functionAddr;
+
+    // 调用成员函数，成员函数默认第一个参数传对象引用
+    std::cout << func(*animatorPtr) << "\n";
+
+    return 0;
+}
